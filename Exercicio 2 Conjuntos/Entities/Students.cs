36dc9cdf839825c5f70efaa380b70f8036a15a70
Isaac Marquetti <isaac.marquetti@gmail.com>
2021-10-20 23:53:42 -0300

    Conjuntos!

---
+++ Exercicio 2 Conjuntos/Entities/Students.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_2_Conjuntos.Entities
+{
+    class Students
+    {
+        public string Name { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Students))
+            {
+                return false;
+            }
+            Students other = obj as Students;
+            return Name.Equals(other.Name);
+        }
+    }
+}
---
+++ Exercicio 2 Conjuntos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Exercicio_2_Conjuntos.Entities;
+
+namespace Exercicio_2_Conjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Students> set = new HashSet<Students>();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write("How many students for course " + i + "? ");
+                int n = int.Parse(Console.ReadLine());
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write("Student " + j + ": ");
+                    string name = Console.ReadLine();
+                    set.Add(new Students { Name = name });
+                }
+            }
+            Console.WriteLine("Total students: " + set.Count);
+        }
+    }
+}
